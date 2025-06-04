a4da4e15dc7f78c552f23a99873df1d092944cec
e.zheltukhin <e.zheltukhin@kt-team.de>
2025-06-04 17:31:03 +0500

    Add Pagination and Workspaces getDocs query

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,41 +1,30 @@
 import { ReactNode } from 'react';
 
-import {
-  dehydrate,
-  HydrationBoundary,
-} from '@tanstack/react-query';
-
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import DocsSideNav from '@/widgets/docs-side-nav';
-
-import { workspacesQueryClientService } from '@/queries/workspaces/api';
 
 export default async function DocsLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const { workspacesListPrefetchQueryClient } = await workspacesQueryClientService.getManyPrefetch();
-
   return (
-    <HydrationBoundary state={dehydrate(workspacesListPrefetchQueryClient)}>
-      <Grid id="docs-layout" container spacing={3} columns={12}>
-        <Grid size={{ xs: 12, sm: 3 }}>
-          <DocsSideNav />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <Box display="flex" flexDirection="column" gap={4}>
-            <Box>
-              Breadcrumbs
-            </Box>
-            {children}
-          </Box>
-        </Grid>
-        <Grid size={{ xs: 12, sm: 3 }}>
-          Some bar
-        </Grid>
+    <Grid id="docs-layout" container spacing={3} columns={12}>
+      <Grid size={{ xs: 12, sm: 3 }}>
+        <DocsSideNav />
       </Grid>
-    </HydrationBoundary>
+      <Grid size={{ xs: 12, sm: 6 }}>
+        <Box display="flex" flexDirection="column" gap={4}>
+          <Box>
+            Breadcrumbs
+          </Box>
+          {children}
+        </Box>
+      </Grid>
+      <Grid size={{ xs: 12, sm: 3 }}>
+        Some bar
+      </Grid>
+    </Grid>
   );
 };
